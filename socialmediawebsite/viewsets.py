8b37b40540257a669e277c295fefd5afbfde129f
Steve Emmanuel <steve.emmanuelck@gmail.com>
2022-06-07 19:02:19 +0530

    basic authentication for api

--- socialmediawebsite/viewsets.py
+++ socialmediawebsite/viewsets.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Post
 from .serializers import PostSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
     queryset = Post.objects.all()
     serializer_class = PostSerializer
