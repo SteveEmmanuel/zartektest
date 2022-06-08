7cc83f1b60268bec2c655b4668b2346f06cc8761
Steve Emmanuel <steve.emmanuelck@gmail.com>
2022-06-08 11:18:04 +0530

    api changed to provide list and also individual post

--- socialmediawebsite/routers.py
+++ socialmediawebsite/routers.py
@@ -2,4 +2,4 @@ from rest_framework import routers
 from .viewsets import PostViewSet
 
 router = routers.DefaultRouter()
-router.register(r'posts', PostViewSet)
+router.register(r'posts', PostViewSet, basename='post')
--- socialmediawebsite/viewsets.py
+++ socialmediawebsite/viewsets.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets
 from rest_framework.authentication import BasicAuthentication
+from django.shortcuts import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 
 from .models import Post
 from .serializers import PostSerializer
+from rest_framework.response import Response
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -12,4 +14,12 @@ class PostViewSet(viewsets.ModelViewSet):
     http_method_names = ['get']
 
     queryset = Post.objects.all()
-    serializer_class = PostSerializer
+
+    def list(self, request):
+        serializer = PostSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        user = get_object_or_404(self.queryset, pk=pk)
+        serializer = PostSerializer(user)
+        return Response(serializer.data)
